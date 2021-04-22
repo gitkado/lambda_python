c676b8d93cc88eb2036db4332b8e8d861c160f86
gitkado <gitkado@gmail.com>
2021-04-22 23:15:18 +0900

    test: dynamodb access function

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM python:3.7.10-buster
-RUN pip install pytest pytest-mock
+RUN pip install boto3 moto pytest pytest-mock pyyaml
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,73 +1,63 @@
-import json
-
+import boto3
 import pytest
+import os
+import yaml
 
-from hello_world import app
+from moto import mock_dynamodb2
+
+
+@pytest.fixture(autouse=True)
+def set_envs(monkeypatch):
+    with open('test_data.yml', 'r', encoding='utf-8') as fp:
+        envs = yaml.safe_load(fp)['environment']
+
+    for k, v in envs.items():
+        monkeypatch.setenv(k, str(v))
 
 
 @pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+def table():
+    with mock_dynamodb2():
+        with open('test_data.yml', 'r', encoding='utf-8') as fp:
+            test_data = yaml.safe_load(fp)['DynamoDB']
 
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+        table_config = test_data['Table']
+        dynamodb = boto3.resource('dynamodb')
+        # テスト用テーブルの作成
+        dynamodb.create_table(
+            TableName=os.environ['TABLE_NAME'],
+            AttributeDefinitions=table_config['AttributeDefinitions'],
+            KeySchema=table_config['KeySchema']
+        )
+        # テスト用データの格納
+        table = dynamodb.Table(os.environ['TABLE_NAME'])
+        with table.batch_writer() as batch:
+            for item in test_data['Items']:
+                batch.put_item(Item=item)
+
+        yield table
 
 
-def test_lambda_handler(apigw_event, mocker):
-
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
-
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+@pytest.mark.parametrize(
+    "event, expected",
+    [
+        (
+            {'body': {'id': '0001'}},
+            [
+                {
+                    'id': '0001',
+                    'name': 'Tanaka',
+                    'age': 100,
+                    'profile': 'human'
+                }
+            ]
+        ),
+        (
+            {'body': {'id': '0005'}},
+            []
+        )
+    ]
+)
+def test_handler(table, event, expected):
+    from hello_world.app import handler
+    assert expected == handler(event, context=None)
